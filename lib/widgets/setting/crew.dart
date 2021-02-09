e025085b2c8a8215ee9e8c3878d06cc7cdd54299
heewookji <wlgmldnr@daum.net>
2021-02-09 13:32:27 +0900

    fix: crew 및 질문 수정

--- lib/widgets/setting/crew.dart
+++ lib/widgets/setting/crew.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../custom_raised_button.dart';
 
@@ -23,7 +24,9 @@ class Crew extends StatelessWidget {
           child: CustomRaisedButton(
             '헤이즐 : UX/UI Designer',
             color: Colors.black,
-            onPressed: () {},
+            onPressed: () async {
+              await launch('https://github.com/Heewookji');
+            },
             alignment: Alignment.centerLeft,
             padding: EdgeInsets.only(left: _screenSize.width * 0.04),
           ),
@@ -33,7 +36,9 @@ class Crew extends StatelessWidget {
           child: CustomRaisedButton(
             '제이 : UX/UI Designer',
             color: Colors.black,
-            onPressed: () {},
+            onPressed: () async {
+              await launch('https://github.com/Heewookji');
+            },
             alignment: Alignment.centerLeft,
             padding: EdgeInsets.only(left: _screenSize.width * 0.04),
           ),
@@ -43,12 +48,26 @@ class Crew extends StatelessWidget {
           child: CustomRaisedButton(
             '우유튀김 : Developer',
             color: Colors.black,
-            onPressed: () {},
+            onPressed: () async {
+              await launch('https://github.com/Heewookji');
+            },
             alignment: Alignment.centerLeft,
             padding: EdgeInsets.only(left: _screenSize.width * 0.04),
           ),
           margin: EdgeInsets.only(bottom: _screenSize.height * 0.02162),
         ),
+//        Container(
+//          child: CustomRaisedButton(
+//            '앤도 라즈 : Sound Designer',
+//            color: Colors.black,
+//            onPressed: () async {
+//              await launch('https://andolaj.com');
+//            },
+//            alignment: Alignment.centerLeft,
+//            padding: EdgeInsets.only(left: _screenSize.width * 0.04),
+//          ),
+//          margin: EdgeInsets.only(bottom: _screenSize.height * 0.02162),
+//        ),
       ],
     );
   }
